065e716d99cebde8cc24192adc936dce4157a216
sidor2 <ilsoldier1984@gmail.com>
2023-07-19 20:23:06 -0700

    Updated stack name.

--- bin/acg-apprentice-labs.ts
+++ bin/acg-apprentice-labs.ts
@@ -5,6 +5,6 @@ import * as cdk from 'aws-cdk-lib';
 import { Ec2InstanceStack } from '../lib/ec2-instance-stack';
 
 const app = new cdk.App();
-new Ec2InstanceStack(app, 'AcgApprenticeLabsStack', {
+new Ec2InstanceStack(app, 'Ec2InstanceStack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
