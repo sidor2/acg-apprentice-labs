d477fa8e322218cf92cea6e76e33c23a750895dc
sidor2 <ilsoldier1984@gmail.com>
2023-07-30 11:23:17 -0700

    Configured NACLs. Configuring SGs.

--- lib/three-tier-network.ts
+++ lib/three-tier-network.ts
@@ -1,11 +1,6 @@
-// solution to a lab from A Cloud Guru
+// solution to labs from A Cloud Guru
 // https://learn.acloud.guru/handson/f2a24706-8b7b-4a21-9ad6-859bd5595215
-
-// Create a VPC with the following CIDR Block Range (10.99.0.0/16)
-// Create six subnets in the VPC you just created: one pair of subnets for the DMZ layer, one pair for the App layer, and one pair for the DB layer. Each pair should be split between AZs.
-// Create a NAT Gateway and provide it with a route to the Internet via the public Route Table
-// Create three NACLs and associate each to one of the subnet groupings (DMZ, App layer, and DB layer subnets).
-
+// https://learn.acloud.guru/handson/c8f78dd1-2c83-4058-b480-cf96a4e78e13
 
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
@@ -22,6 +17,74 @@ export class ThreeTierNetworkStack extends cdk.Stack {
             enableDnsSupport: true,
             tags: [{ key: 'Name', value: 'SysOpsVPC' }],
             instanceTenancy: 'default'
+        });
+
+        // create security groups for each layer
+        const BastionSecurityGroup = new ec2.CfnSecurityGroup(this, 'BastionSecurityGroup', {
+            groupName: 'BastionSecurityGroup',
+            groupDescription: 'BastionSecurityGroup',
+            vpcId: vpc.ref,
+            securityGroupIngress: [
+                {
+                    ipProtocol: 'tcp',
+                    fromPort: 22,
+                    toPort: 22,
+                    cidrIp: '0.0.0.0/0'
+                }
+            ],
+            securityGroupEgress: [],
+            tags: [{ key: 'Name', value: 'BastionSecurityGroup' }]
+        });
+
+        const ALBSecurityGroup = new ec2.CfnSecurityGroup(this, 'ALBSecurityGroup', {
+            groupName: 'ALBSecurityGroup',
+            groupDescription: 'ALBSecurityGroup',
+            vpcId: vpc.ref,
+            securityGroupIngress: [
+                {
+                    ipProtocol: 'tcp',
+                    fromPort: 80,
+                    toPort: 80,
+                    cidrIp: '0.0.0.0/0'
+                }
+            ],
+            tags: [{ key: 'Name', value: 'ALBSecurityGroup' }]
+        });
+
+        const appSecurityGroup = new ec2.CfnSecurityGroup(this, 'AppSecurityGroup', {
+            groupName: 'AppSecurityGroup',
+            groupDescription: 'AppSecurityGroup',
+            vpcId: vpc.ref,
+            securityGroupIngress: [
+                {
+                    ipProtocol: 'tcp',
+                    fromPort: 80,
+                    toPort: 80,
+                    sourceSecurityGroupId: ALBSecurityGroup.attrGroupId
+                },
+                {
+                    ipProtocol: 'tcp',
+                    fromPort: 22,
+                    toPort: 22,
+                    sourceSecurityGroupId: BastionSecurityGroup.attrGroupId
+                }
+            ],
+            tags: [{ key: 'Name', value: 'AppSecurityGroup' }]
+        });
+
+        const dbSecurityGroup = new ec2.CfnSecurityGroup(this, 'DbSecurityGroup', {
+            groupName: 'DbSecurityGroup',
+            groupDescription: 'DbSecurityGroup',
+            vpcId: vpc.ref,
+            securityGroupIngress: [
+                {
+                    ipProtocol: 'tcp',
+                    fromPort: 3306,
+                    toPort: 3306,
+                    sourceSecurityGroupId: appSecurityGroup.attrGroupId
+                }
+            ],
+            tags: [{ key: 'Name', value: 'DbSecurityGroup' }]
         });
 
         // Create six subnets in the VPC you just created: one pair of subnets for the DMZ layer, one pair for the App layer, and one pair for the DB layer. Each pair should be split between AZs.
@@ -105,7 +168,254 @@ export class ThreeTierNetworkStack extends cdk.Stack {
             gatewayId: igw.ref
         });
 
+        const privateRouteTable = new ec2.CfnRouteTable(this, 'SysOpsPrivateRouteTable', {
+            vpcId: vpc.ref,
+            tags: [{ key: 'Name', value: 'SysOpsPrivateRouteTable' }]
+        });
 
+        const privateRoute = new ec2.CfnRoute(this, 'SysOpsPrivateRoute', {
+            routeTableId: privateRouteTable.ref,
+            destinationCidrBlock: '0.0.0.0/0',
+            natGatewayId: natGateway.ref
+        });
+
+        // Associate the public Route Table with the DMZ subnets and the private Route Table with the App and DB subnets.
+        const dmzSubnet1RouteTableAssociation = new ec2.CfnSubnetRouteTableAssociation(this, 'DMZSubnet1RouteTableAssociation', {
+            subnetId: dmzSubnet1.ref,
+            routeTableId: publicRouteTable.ref
+        });
+
+        const dmzSubnet2RouteTableAssociation = new ec2.CfnSubnetRouteTableAssociation(this, 'DMZSubnet2RouteTableAssociation', {
+            subnetId: dmzSubnet2.ref,
+            routeTableId: publicRouteTable.ref
+        });
+
+        const appSubnet1RouteTableAssociation = new ec2.CfnSubnetRouteTableAssociation(this, 'AppSubnet1RouteTableAssociation', {
+            subnetId: appSubnet1.ref,
+            routeTableId: privateRouteTable.ref
+        });
+
+        const appSubnet2RouteTableAssociation = new ec2.CfnSubnetRouteTableAssociation(this, 'AppSubnet2RouteTableAssociation', {
+            subnetId: appSubnet2.ref,
+            routeTableId: privateRouteTable.ref
+        });
+
+        const dbSubnet1RouteTableAssociation = new ec2.CfnSubnetRouteTableAssociation(this, 'DBSubnet1RouteTableAssociation', {
+            subnetId: dbSubnet1.ref,
+            routeTableId: privateRouteTable.ref
+        });
+
+        const dbSubnet2RouteTableAssociation = new ec2.CfnSubnetRouteTableAssociation(this, 'DBSubnet2RouteTableAssociation', {
+            subnetId: dbSubnet2.ref,
+            routeTableId: privateRouteTable.ref
+        });
+
+        // Create three NACLs and associate each to one of the subnet groupings (DMZ, App layer, and DB layer subnets).
+        const dmzNacl = new ec2.CfnNetworkAcl(this, 'DMZNacl', {
+            vpcId: vpc.ref,
+            tags: [{ key: 'Name', value: 'DMZNacl' }]
+        });
+
+        const dmzNaclEntry1 = new ec2.CfnNetworkAclEntry(this, 'DMZNaclEntry1', {
+            networkAclId: dmzNacl.ref,
+            ruleNumber: 100,
+            protocol: 6,
+            ruleAction: 'allow',
+            egress: false,
+            cidrBlock: '0.0.0.0/0',
+            portRange: { from: 22, to: 22 }
+        });
+
+        const dmzNaclEntry2 = new ec2.CfnNetworkAclEntry(this, 'DMZNaclEntry2', {
+            networkAclId: dmzNacl.ref,
+            ruleNumber: 100,
+            protocol: 6,
+            ruleAction: 'allow',
+            egress: true,
+            cidrBlock: '0.0.0.0/0',
+            portRange: { from: 22, to: 22 }
+        });
+
+        const dmzNaclEntry3 = new ec2.CfnNetworkAclEntry(this, 'DMZNaclEntry3', {
+            networkAclId: dmzNacl.ref,
+            ruleNumber: 110,
+            protocol: 6,
+            ruleAction: 'allow',
+            egress: false,
+            cidrBlock: '0.0.0.0/0',
+            portRange: { from: 80, to: 80 }
+        });
+
+        const dmzNaclEntry4 = new ec2.CfnNetworkAclEntry(this, 'DMZNaclEntry4', {
+            networkAclId: dmzNacl.ref,
+            ruleNumber: 110,
+            protocol: 6,
+            ruleAction: 'allow',
+            egress: true,
+            cidrBlock: '0.0.0.0/0',
+            portRange: { from: 80, to: 80 }
+        });
+
+        const dmzNaclEntry5 = new ec2.CfnNetworkAclEntry(this, 'DMZNaclEntry5', {
+            networkAclId: dmzNacl.ref,
+            ruleNumber: 120,
+            protocol: 6,
+            ruleAction: 'allow',
+            egress: false,
+            cidrBlock: '0.0.0.0/0',
+            portRange: { from: 443, to: 443 }
+        });
+
+        const dmzNaclEntry6 = new ec2.CfnNetworkAclEntry(this, 'DMZNaclEntry6', {
+            networkAclId: dmzNacl.ref,
+            ruleNumber: 120,
+            protocol: 6,
+            ruleAction: 'allow',
+            egress: true,
+            cidrBlock: '0.0.0.0/0',
+            portRange: { from: 443, to: 443 }
+        });
+
+        const dmzNaclEntry7 = new ec2.CfnNetworkAclEntry(this, 'DMZNaclEntry7', {
+            networkAclId: dmzNacl.ref,
+            ruleNumber: 130,
+            protocol: 6,
+            ruleAction: 'allow',
+            egress: false,
+            cidrBlock: '0.0.0.0/0',
+            portRange: { from: 1024, to: 65535 }
+        });
+
+        const dmzNaclEntry8 = new ec2.CfnNetworkAclEntry(this, 'DMZNaclEntry8', {
+            networkAclId: dmzNacl.ref,
+            ruleNumber: 130,
+            protocol: 6,
+            ruleAction: 'allow',
+            egress: true,
+            cidrBlock: '0.0.0.0/0',
+            portRange: { from: 1024, to: 65535 }
+        });
+
+        const appNacl = new ec2.CfnNetworkAcl(this, 'AppNacl', {
+            vpcId: vpc.ref,
+            tags: [{ key: 'Name', value: 'AppNacl' }]
+        });
+
+        const appNaclEntry1 = new ec2.CfnNetworkAclEntry(this, 'AppNaclEntry1', {
+            networkAclId: appNacl.ref,
+            ruleNumber: 100,
+            protocol: 6,
+            ruleAction: 'allow',
+            egress: false,
+            cidrBlock: '0.0.0.0/0',
+            portRange: { from: 80, to: 80 }
+        });
+
+        const appNaclEntry2 = new ec2.CfnNetworkAclEntry(this, 'AppNaclEntry2', {
+            networkAclId: appNacl.ref,
+            ruleNumber: 100,
+            protocol: 6,
+            ruleAction: 'allow',
+            egress: true,
+            cidrBlock: '0.0.0.0/0',
+            portRange: { from: 80, to: 80 }
+        });
+
+        const appNaclEntry3 = new ec2.CfnNetworkAclEntry(this, 'AppNaclEntry3', {
+            networkAclId: appNacl.ref,
+            ruleNumber: 110,
+            protocol: 6,
+            ruleAction: 'allow',
+            egress: false,
+            cidrBlock: '0.0.0.0/0',
+            portRange: { from: 1024, to: 65535 }
+        });
+
+        const appNaclEntry4 = new ec2.CfnNetworkAclEntry(this, 'AppNaclEntry4', {
+            networkAclId: appNacl.ref,
+            ruleNumber: 110,
+            protocol: 6,
+            ruleAction: 'allow',
+            egress: true,
+            cidrBlock: '0.0.0.0/0',
+            portRange: { from: 1024, to: 65535 }
+        });
+
+        const dbNacl = new ec2.CfnNetworkAcl(this, 'DBNacl', {
+            vpcId: vpc.ref,
+            tags: [{ key: 'Name', value: 'DBNacl' }]
+        });
+
+        const dbNaclEntry1 = new ec2.CfnNetworkAclEntry(this, 'DBNaclEntry1', {
+            networkAclId: dbNacl.ref,
+            ruleNumber: 100,
+            protocol: 6,
+            ruleAction: 'allow',
+            egress: false,
+            cidrBlock: '0.0.0.0/0',
+            portRange: { from: 3306, to: 3306 }
+        });
+
+        const dbNaclEntry2 = new ec2.CfnNetworkAclEntry(this, 'DBNaclEntry2', {
+            networkAclId: dbNacl.ref,
+            ruleNumber: 100,
+            protocol: 6,
+            ruleAction: 'allow',
+            egress: true,
+            cidrBlock: '0.0.0.0/0',
+            portRange: { from: 3306, to: 3306 }
+        });
+
+        const dbNaclEntry3 = new ec2.CfnNetworkAclEntry(this, 'DBNaclEntry3', {
+            networkAclId: dbNacl.ref,
+            ruleNumber: 110,
+            protocol: 6,
+            ruleAction: 'allow',
+            egress: false,
+            cidrBlock: '0.0.0.0/0',
+            portRange: { from: 1024, to: 65535 }
+        });
+
+        const dbNaclEntry4 = new ec2.CfnNetworkAclEntry(this, 'DBNaclEntry4', {
+            networkAclId: dbNacl.ref,
+            ruleNumber: 110,
+            protocol: 6,
+            ruleAction: 'allow',
+            egress: true,
+            cidrBlock: '0.0.0.0/0',
+            portRange: { from: 1024, to: 65535 }
+        });
+
+        // Associate the NACLs with the appropriate subnets
+        const dmzNaclAssociation1 = new ec2.CfnSubnetNetworkAclAssociation(this, 'DMZNaclAssociation1', {
+            subnetId: dmzSubnet1.ref,
+            networkAclId: dmzNacl.ref
+        });
+
+        const dmzNaclAssociation2 = new ec2.CfnSubnetNetworkAclAssociation(this, 'DMZNaclAssociation2', {
+            subnetId: dmzSubnet2.ref,
+            networkAclId: dmzNacl.ref
+        });
+
+        const appNaclAssociation1 = new ec2.CfnSubnetNetworkAclAssociation(this, 'AppNaclAssociation1', {
+            subnetId: appSubnet1.ref,
+            networkAclId: appNacl.ref
+        });
+
+        const appNaclAssociation2 = new ec2.CfnSubnetNetworkAclAssociation(this, 'AppNaclAssociation2', {
+            subnetId: appSubnet2.ref,
+            networkAclId: appNacl.ref
+        });
+
+        const dbNaclAssociation1 = new ec2.CfnSubnetNetworkAclAssociation(this, 'DBNaclAssociation1', {
+            subnetId: dbSubnet1.ref,
+            networkAclId: dbNacl.ref
+        });
+
+        const dbNaclAssociation2 = new ec2.CfnSubnetNetworkAclAssociation(this, 'DBNaclAssociation2', {
+            subnetId: dbSubnet2.ref,
+            networkAclId: dbNacl.ref
+        });
 
     }
 }
